6fcefebe91ae405e41b68ebfc7cb728c96fa8e64
YuriSarmanho <yuri.sarmanho@hotmail.com>
2021-07-19 15:54:15 -0300

    divisão de resposabilidades através das classes

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-//Colocar todas essa funções em um arquivo responsável e refatorar o código
+
 class App{
     constructor(){
         this.cpf = document.getElementById("cpf")
         this.cardNumber = document.getElementById("CardNumber")
         this.dueDate = document.getElementById("DueDate")
+
         this.specialCaracters = new SpecialCaracters();
     }
-    implementCpfCaracters(){
-        if(cpf.value.length == 3 || cpf.value.length == 7){
-            return cpf.value += "."
-        }else if(cpf.value.length == 11){
-            return cpf.value += "-"
-        }    
+
+    AddCpfCaracters(){
+        this.specialCaracters.implementCpfCaracters(cpf);
     }
-    cardNumberSpace(){
-        if(CardNumber.value.length == 4 ||CardNumber.value.length == 9 ||CardNumber.value.length == 14){
-            return CardNumber.value += " "
-        }
+    AddCardNumberSpace(){
+        this.specialCaracters.cardNumberSpace(CardNumber);
     }
-    DueDate(){
-        if(DueDate.value.length == 2){
-            return DueDate.value += "/"
-        }
+    AddDueDateCaracter(){
+        this.specialCaracters.DueDate(this.DueDate)
     }
+
 }
 const app = new App();
--- src/spaceAndCaracteres.js
+++ src/spaceAndCaracteres.js
@@ -1,7 +1,23 @@
 class SpecialCaracters{
     constructor(){
-        this.cpf = document.getElementById("cpf")
-        
+        this.Helper = new Helper();
+    }
+    implementCpfCaracters(cpf){
+        if(this.Helper.isDashPosition(cpf)){
+            return cpf.value += "."
+        }else if(this.Helper.isDotPosition(cpf)){
+            return cpf.value += "-"
+        }    
+    }
+    cardNumberSpace(){
+        if(this.Helper.isCardNumberSpace(CardNumber)){
+            return CardNumber.value += " "
+        }
+    }
+    DueDate(){
+        if(this.Helper.isBackSlashPosition(DueDate)){
+            return DueDate.value += "/"
+        }
     }
     
 }
